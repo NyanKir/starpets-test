25a3b38d4f769c8e103d01eb7213f8481e8095c9
Sytnaynskiy Kirill <nyan.kirill@gmail.com>
2023-10-22 14:24:41 +0300

    feat: change to serial

--- migrations/migrations/up.sql
+++ migrations/migrations/up.sql
@@ -1,5 +1,5 @@
 CREATE TABLE public.users (
-  user_id BIGINT NOT NULL,
+  user_id SERIAL NOT NULL,
   balance INT NOT NULL CHECK (balance >= 0),
   created_at TIMESTAMP NOT NULL DEFAULT now(),
   CONSTRAINT pk_users_user_id PRIMARY KEY (user_id)
